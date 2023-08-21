87a0a575d218f11c50487a1ab7ce5aa9b807ebd6
ruhancs <ruhan_cs@hotmail.com>
2023-08-21 05:24:54 -0300

    feat:adicionado config iniciais nestjs

---
+++ src/nest/jest.config.ts
@@ -0,0 +1,20 @@
+export default {
+    displayName: {
+        name: 'nest',
+        color: 'magentaBright'
+      },
+    moduleFileExtensions: ["js","json","ts"],
+    rootDir: "src",
+    testRegex: ".*\\.spec\\.ts$",
+    transform: {
+        "^.+\\.(t|j)s$": "@swc/jest"
+    },
+    collectCoverageFrom: [
+        "**/*.(t|j)s"
+    ],
+    coverageDirectory: "../coverage",
+    testEnvironment: "node",
+    moduleNameMapper: {
+        "@rc/micro\\-videos/(.*)$": "<rootDir>/../../../node_modules/@fc/micro-videos/dist/$1",
+    }
+};
