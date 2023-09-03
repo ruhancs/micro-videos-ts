5ba2d51fed1dc26b8e1729cf5b938b31a1745635
ruhancs <ruhan_cs@hotmail.com>
2023-09-03 11:42:03 -0300

    refact: ajustado docker

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,10 +1,24 @@
 #!bin/bash
 
-if [ ! -f "./src/@core/.env.testing"]; then
+if [ ! -f "./src/@core/.env.test"]; then
     cp ./src/@core/.env.test.example ./src/@core/.env.test
 fi
 
+if [ ! -f "./src/nest/envs/.env"]; then
+    cp ./src/@core/.env.example ./src/@core/.env
+fi
+
+if [ ! -f "./src/nest/envs/.env.test"]; then
+    cp ./src/@core/.env.test.example ./src/@core/.env.test
+fi
+
+if [ ! -f "./src/nest/envs/.env.e2e"]; then
+    cp ./src/@core/.env.e2e.example ./src/@core/.env.e2e
+fi
+
 npm install
+
+npm run build -w @rc/micro-videos
 
 tail -f /dev/null #adicionar permissao no terminal: chmod +x .docker/start.sh
 
